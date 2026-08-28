a6b4448639d20b1c6132660deaeb4975522ee23f
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-08-28 02:38:27 -0300

    alterando logo da navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     <>
       <nav className={styles.navbar}>
         <img
-          src="../src/assets/wwe_universe.png"
+          src="../../public/wwe_universe.png"
           alt="Logo do WWE 2K26"
           width="150px"
           className={styles.logo}
